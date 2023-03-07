9501b1245752a24a31530e2d1ef8e5e071c3c67c
ysl0628 <yihsinlan@gmail.com>
2023-03-07 11:25:54 +0800

    feat: create controller service module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { ReportsService } from './reports/reports.service';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ReportsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ReportsService],
 })
 export class AppModule {}
