383c947aca3d9a1e76482686ef2b0c53347d9409
ysl0628 <yihsinlan@gmail.com>
2023-03-08 09:20:50 +0800

    feat: connect db to root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { ReportsService } from './reports/reports.service';
 
+import { User } from './users/users.entity';
+import { Report } from './reports/reports.entity';
+
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true,
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, ReportsService],
 })
