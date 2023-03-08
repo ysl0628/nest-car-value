d4b98a1656ee9270e61108087a5f316756f3e0ce
ysl0628 <yihsinlan@gmail.com>
2023-03-08 13:13:37 +0800

    feat: implement create and save user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  // 由於 DI 無法有效的讀取泛型 Repository<User> 所以必須定義 @InjectRepository(User)
+  constructor(@InjectRepository(User) private repo: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+}
