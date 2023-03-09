40182a9c2c380f1eef0850ad73f8992cd6d28d76
ysl0628 <yihsinlan@gmail.com>
2023-03-09 13:24:08 +0800

    feat: replace NotFoundException with Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
@@ -25,7 +25,7 @@ export class UsersService {
   async update(id: number, attr: Partial<User>) {
     const user = await this.repo.findOneBy({ id });
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
 
     const newUser = { ...user, ...attr };
@@ -35,7 +35,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.repo.findOneBy({ id });
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
 
     return this.repo.remove(user);
