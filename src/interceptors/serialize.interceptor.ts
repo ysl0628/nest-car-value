04db44a718bfd60d97f5d00b90f3cee6ebff6f69
ysl0628 <yihsinlan@gmail.com>
2023-03-10 13:40:58 +0800

    feat: create custom interceptor in interceptors

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  NestInterceptor,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ClassConstructor, plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+// UseInterceptors decorator 太冗長了，自己寫一個 decorator
+// decorator 是以 function 建立
+export function Serialize<T>(dto: ClassConstructor<T>) {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: ClassConstructor<T>) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    // Run something before a request is handled
+    // by the request handler
+
+    return next.handle().pipe(
+      map((data: T) => {
+        // Run something before the response is sent out
+        return plainToInstance(this.dto, data, {
+          // 為了防止 UserDto 中有其他非定義於 data 中的屬性一起被輸出至 response 中
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
