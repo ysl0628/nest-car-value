5c2dc51e0767342fd1a84cd34d66e1e4f0f3b304
ysl0628 <yihsinlan@gmail.com>
2023-03-10 13:48:40 +0800

    feat: implement user interceptor in user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,10 +9,13 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
@@ -22,9 +25,14 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  // 從 url 傳來的所有變數皆為 string，所以在傳入 service 時需轉換型別
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
+  // 以自定義的 decorator 代替
+  // @Serialize(UserDto)
   @Get('/:id')
+  // 從 url 傳來的所有變數皆為 string，所以在傳入 service 時需轉換型別
   async findUser(@Param('id') id: string) {
+    console.log('handler is running');
+
     const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
@@ -33,6 +41,7 @@ export class UsersController {
     return user;
   }
 
+  // @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
