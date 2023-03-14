8d0675df11576fc187bf4c00e32011a5a3d5cfe9
ysl0628 <yihsinlan@gmail.com>
2023-03-14 11:55:24 +0800

    feat: implement globally scoped interceptors

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,17 @@ import { User } from './users.entity';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { CurrentUserInterceptor } from 'src/interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, AuthService],
+  providers: [
+    UsersService,
+    AuthService,
+    // globally scoped interceptors
+    { provide: APP_INTERCEPTOR, useClass: CurrentUserInterceptor },
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}
